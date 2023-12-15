8a2a28d93afa4bb647d643248d1b4d9f1c74b146
ArnBon <bonillaarnaldo@gmail.com>
2023-12-15 19:38:50 -0400

    clase 236 primeros pasos con formularios reactivos

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class BasicPageComponent {
 
+  constructor(private fb: FormBuilder){}
+
+  /**FORMA 1 */
+  /*public myForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    price: new FormControl(0),
+    inStorage: new FormControl(0),
+  });*/
+
+  /**FORMA 2 */
+  public myForm: FormGroup = this.fb.group({
+    name: [''],
+    price: [0],
+    inStorage: [0],
+  });
+
+  onSave(): void {
+    console.log(this.myForm.value);
+  }
 }
