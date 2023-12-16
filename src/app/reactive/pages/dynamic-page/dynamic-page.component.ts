d32f54ad3bae22a78641c08f00391ce5bfebc22f
ArnBon <bonillaarnaldo@gmail.com>
2023-12-16 10:25:35 -0400

    clase 241 formulario dinamico

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class DynamicPageComponent {
 
-}
+  // public myForm2 = new FormGroup({
+  //   favoriteGames: new FormArray([])
+  // });
+
+    constructor(private fb:FormBuilder){}
+
+    public myForm:FormGroup = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      favoriteGames: this.fb.array([
+        ['Metal Gear', Validators.required],
+        ['Assasins Creed', Validators.required],
+      ])
+    });
+
+    onSubmit(): void {
+      if(this.myForm.invalid){
+        this.myForm.markAllAsTouched();
+        return;
+      }
+      console.log(this.myForm.value)
+      this.myForm.reset();
+    }
+
+    get favoriteGames(){
+      return this.myForm.get('favoriteGames') as FormArray;
+    }
+
+
+  }
+
+
