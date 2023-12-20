dcb6f6bea8337de187dfbb3a37cb4a4b23b981c8
ArnBon <bonillaarnaldo@gmail.com>
2023-12-20 18:55:44 -0400

    clase 255 validadores asyncronos

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { cantBeStrider } from 'src/app/shared/validators/validators';
 // import * as customValidators from '../../../shared/validators/validators';
 import { ValidatorsService } from 'src/app/shared/service/validators.service';
+import { EmailValidator } from '../../../shared/validators/email-validator.service';
+
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -13,7 +15,8 @@ export class RegisterPageComponent {
 
   constructor(
     private fb: FormBuilder,
-    private validatorsService: ValidatorsService
+    private validatorsService: ValidatorsService,
+    private emailValidator: EmailValidator
     ){}
 
   onSubmit(){
@@ -22,7 +25,9 @@ export class RegisterPageComponent {
 
   public myForm: FormGroup = this.fb.group({
     name: ['', [ Validators.required, Validators.pattern( this.validatorsService.firstNameAndLastnamePattern )  ]],
-    email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern ) ]],
+    // email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern ) ]],
+    // email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern )], [ new EmailValidator ]],
+    email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern )], [ this.emailValidator ]],
     username: ['', [ Validators.required, this.validatorsService.cantBeStrider ]],
     password: ['', [ Validators.required, Validators.minLength(6) ]],
     password2: ['', [ Validators.required ]],
