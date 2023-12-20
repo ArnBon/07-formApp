981e2fc63c9c01f551371de58cc4ba831da1df27
ArnBon <bonillaarnaldo@gmail.com>
2023-12-20 19:22:18 -0400

    clase 257 verificar que dos campos sean iguales

--- src/app/shared/service/validators.service.ts
+++ src/app/shared/service/validators.service.ts
@@ -25,7 +25,7 @@ export class ValidatorsService {
     return form.controls[field].errors && form.controls[field].touched;
   }
 
-
+// clase 257
   public isFieldOneEqualFieldTwo( field1: string, field2: string ) {
 
     return ( formGroup: AbstractControl ): ValidationErrors | null => {
@@ -43,6 +43,34 @@ export class ValidatorsService {
     }
 
   }
+
+  /**
+   * #### **Explicación de la Función de Validación isFieldOneEqualFieldTwo**
+
+1. **Parámetros de Entrada**
+   - La función `isFieldOneEqualFieldTwo` toma dos parámetros de tipo `string` llamados `field1` y `field2`, que representan los nombres de los campos que se van a comparar.
+
+2. **Retorno de una Función de Validación**
+   - La función principal devuelve una función anónima que toma un parámetro `formGroup` de tipo `AbstractControl` (presumiblemente un FormGroup) y devuelve un objeto de tipo `ValidationErrors` o `null`.
+
+3. **Obtención de Valores de los Campos**
+   - Se obtienen los valores de los campos especificados (`field1` y `field2`) del `formGroup` utilizando `formGroup.get(field1)?.value` y `formGroup.get(field2)?.value`.
+
+4. **Comparación de Valores**
+   - Se compara si los valores de los dos campos son diferentes utilizando la expresión `fieldValue1 !== fieldValue2`.
+   - Si los valores son diferentes, se establece un error en el `field2` indicando que los campos no son iguales mediante `formGroup.get(field2)?.setErrors({ notEqual: true })`, y se devuelve un objeto de error `{ notEqual: true }`.
+
+5. **Eliminación de Errores**
+   - Si los valores son iguales, se eliminan los errores del `field2` utilizando `formGroup.get(field2)?.setErrors(null)`.
+
+6. **Retorno de Resultados de Validación**
+   - Se devuelve `null` si los valores son iguales, lo que indica que la validación ha tenido éxito.
+
+En resumen, esta función de validación comprueba si los valores de dos campos en un formulario son iguales. Si los valores son diferentes, se establece un error en el segundo campo y se devuelve un objeto de error. Si los valores son iguales, se eliminan los errores del segundo campo y se devuelve `null`.
+
+Espero que esta explicación aclare el funcionamiento de la función de validación. Si tienes más preguntas al respecto, no dudes en preguntar.
+   *
+   */
 
 
 }
