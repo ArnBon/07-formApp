9c1fcf4679bba3f83a0a1186e3e4f25b50caec6c
ArnBon <bonillaarnaldo@gmail.com>
2023-12-20 18:14:28 -0400

    clase 253 validaciones de email y patrones

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { cantBeStrider } from 'src/app/shared/validators/validators';
+import * as customValidators from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -16,10 +17,13 @@ export class RegisterPageComponent {
   }
 
   public myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.required]],
+    // name: ['', [Validators.required]],
+    name: ['', [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
+    // email: ['', [Validators.required, Validators.email]],
+    email: ['', [Validators.required, Validators.pattern(customValidators.emailPattern)]],
     // username: ['', [Validators.required]],
-    username: ['', [Validators.required, cantBeStrider ]],
+    // username: ['', [Validators.required, cantBeStrider ]],
+    username: ['', [Validators.required, customValidators.cantBeStrider ]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]],
   });
