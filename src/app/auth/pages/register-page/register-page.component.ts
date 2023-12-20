12700fed572753dac50001e626494ef2b6341fa9
ArnBon <bonillaarnaldo@gmail.com>
2023-12-20 18:33:40 -0400

    clase 254 Validators Service

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { cantBeStrider } from 'src/app/shared/validators/validators';
-import * as customValidators from '../../../shared/validators/validators';
+// import * as customValidators from '../../../shared/validators/validators';
+import { ValidatorsService } from 'src/app/shared/service/validators.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -10,26 +11,25 @@ import * as customValidators from '../../../shared/validators/validators';
 })
 export class RegisterPageComponent {
 
-  constructor(private fb: FormBuilder){}
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService
+    ){}
 
   onSubmit(){
     this.myForm.markAllAsTouched();
   }
 
   public myForm: FormGroup = this.fb.group({
-    // name: ['', [Validators.required]],
-    name: ['', [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
-    // email: ['', [Validators.required, Validators.email]],
-    email: ['', [Validators.required, Validators.pattern(customValidators.emailPattern)]],
-    // username: ['', [Validators.required]],
-    // username: ['', [Validators.required, cantBeStrider ]],
-    username: ['', [Validators.required, customValidators.cantBeStrider ]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
-    password2: ['', [Validators.required]],
+    name: ['', [ Validators.required, Validators.pattern( this.validatorsService.firstNameAndLastnamePattern )  ]],
+    email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern ) ]],
+    username: ['', [ Validators.required, this.validatorsService.cantBeStrider ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required ]],
   });
 
   isValidField(field: string){
-
+    return this.validatorsService.isValidField(this.myForm, field);
   }
 
 }
